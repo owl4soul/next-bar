40520929af7c021f65872fba426c31cb3fca8a91
owl4soul <resuuser989@gmail.com>
2019-04-06 23:14:18 +0300

    Update calculator to work with NULL-ingredient-drink - made it count price of such drink as zero and return 0 instead of prime cost of the drink

--- src/main/java/bar/tools/Calculator.java
+++ src/main/java/bar/tools/Calculator.java
@@ -1,5 +1,6 @@
 package bar.tools;
 
+import bar.products.Ingredient;
 import bar.products.Product;
 import bar.products.drinks.Drink;
 
@@ -9,6 +10,12 @@ public abstract class Calculator {
     public static Integer calcPrice(Product product) {
         int addedCost = product.getCost();
         int primeCost = 0;
+
+        //here we set the price to the NULL-ingredient-drink to zero instead of it's prime cost
+        if (((Drink) product).consist.containsKey(Ingredient.NULL)) {
+            return 0;
+        }
+
 
         for (Map.Entry entry : ((Drink) product).consist.entrySet()) {
             Product p = (Product) entry.getKey();
